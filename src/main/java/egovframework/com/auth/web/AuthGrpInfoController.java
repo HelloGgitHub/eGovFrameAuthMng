cf6c09dd583a13ef46a2f5319b61108eb9a177c4
ywgo <jungun8309@gmail.com>
2020-06-05 10:31:42 +0900

    fff

--- src/main/java/egovframework/com/auth/web/AuthGrpInfoController.java
+++ src/main/java/egovframework/com/auth/web/AuthGrpInfoController.java
@@ -33,7 +33,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 @RestController
-@Api(value = "AuthgrpinfoController", description = "권한그룹 정보 관리 REST APddddddddddI")
+@Api(value = "AuthgrpinfoController", description = "권한그룹 정보 관리 REST APddddddddddddddI")
 @RequestMapping("/authgrp")
 public class AuthGrpInfoController {
 	
